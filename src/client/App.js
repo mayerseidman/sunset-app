c43d33ca60de18891b84505062b2c8a3fd406222
mayerseidman <mzseidman@gmail.com>
2019-06-19 15:03:29 -0700

    puts in image search and spit back

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
 import './app.css';
 import ReactImage from './react.png';
+import ImageUploader from 'react-images-upload';
 
 export default class App extends Component {
-  state = { username: null };
+    constructor(props) {
+            super(props);
+             this.state = { pictures: [], username: null, imgName: null, results: null };
+    }
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+    componentDidMount() {
+        localStorage.setItem("item", "value");
+        fetch('/api/getUsername')
+        .then(res => res.json())
+        .then(user => this.setState({ username: user.username }));
+    }
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
+    onDrop(picture) {
+        var imgName = picture[0].name;
+        console.log(picture[0])
+        this.setState({
+            pictures: this.state.pictures.concat(picture),
+            imgName: imgName,
+        });
+    }
+
+    extractText() {
+        fetch('api/extractText?imgName=' + this.state.imgName)
+        .then(res => res.json())
+        .then(image => this.setState({ results: image.text }))
+    }
+
+    render() {
+        const { username } = this.state;
+        return (
+            <div>
+                <ImageUploader
+                    withIcon={ true }
+                    buttonText='Choose images'
+                    onChange={ this.onDrop.bind(this) }
+                    imgExtension={ ['.jpg', '.gif', '.png', '.gif'] }
+                    maxFileSize={ 5242880 }
+                  />
+                {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+                <p>HIP HIP HOORAY</p>
+                <a onClick={ this.extractText.bind(this) }>GET TEXT FROM IMAGE</a>
+                <p>{ this.state.results }</p>
+            </div>
+        );
+    }
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,61 @@
+
 const express = require('express');
 const os = require('os');
-
 const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/api/extractText', function(req, res) {
+	var imgName = req.query.imgName;
+	console.log("EXTRACTTTTTTTTTTTTTTTT", imgName)
+
+	const ocrSpaceApi = require('ocr-space-api');
+
+	var options =  { 
+	    apikey: '8f5d685ff588957',
+	    language: 'por', // Português
+	    imageFormat: 'image/png', // Image Type (Only png ou gif is acceptable at the moment i wrote this)
+	    isOverlayRequired: true
+	};
+
+	// Image file to upload
+	// Running off of my desktop for now....have to make this eventually read off database...
+	const imageFilePath = "/Users/mayerseidman/Desktop/" + imgName + ""
+
+	// Run and wait the result
+	ocrSpaceApi.parseImageFromLocalFile(imageFilePath, options)
+	  .then(function (parsedResult) {
+	    console.log('parsedText: \n', parsedResult.parsedText);
+	    res.send({ text: parsedResult.parsedText })
+	    console.log('ocrParsedResult: \n', parsedResult.ocrParsedResult);
+	  }).catch(function (err) {
+	    console.log('ERROR:', err);
+	  });
+
+})
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+
+
+
+// const ocrSpaceApi = require('ocr-space-api');
+
+// var options =  { 
+//     apikey: '8f5d685ff588957',
+//     language: 'por', // Português
+//     imageFormat: 'image/png', // Image Type (Only png ou gif is acceptable at the moment i wrote this)
+//     isOverlayRequired: true
+// };
+
+
+// // Image file to upload
+// const imageFilePath = "/Users/mayerseidman/Desktop/imageFile.png";
+
+// // Run and wait the result
+// ocrSpaceApi.parseImageFromLocalFile(imageFilePath, options)
+//   .then(function (parsedResult) {
+//     console.log('parsedText: \n', parsedResult.parsedText);
+//     console.log('ocrParsedResult: \n', parsedResult.ocrParsedResult);
+//   }).catch(function (err) {
+//     console.log('ERROR:', err);
+//   });
