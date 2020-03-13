4add725a5941f8cb11ccfda52e88f420acdd8ce8
mayerseidman <mzseidman@gmail.com>
2020-03-12 18:39:44 -0700

    changes

--- go_live.sh
+++ go_live.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo "Bon Voyage ................🚢................"
 echo "Here comes Github commands 🐱"
+
 git status
 git add -A 
 git commit -m "changes"
 git push origin master
+
 echo "Time to build 🏗️"
 npm run build
-echo "Lets get it on Heroku 🔥"
+echo "Lets get it on Heroku ⛰️"
 git push heroku master
