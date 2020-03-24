b1e2c4ae35ec261257d6fc4b8280b6b43c7e2a5b
mayerseidman <mzseidman@gmail.com>
2020-03-23 17:57:47 -0700

    changes

--- src/server/index.js
+++ src/server/index.js
@@ -336,114 +336,16 @@ app.post('/api/submit-form', function (req, res) {
 	// 	// console.log("Result from MONK", result[0].phone_number)
 	// 	console.log("Result from MONK", result)
 	// })
-
-	const mongodb = require('mongodb');
-
-// Create seed data
-
-let seedData = [
-  {
-    decade: '1970s',
-    artist: 'Debby Boone',
-    song: 'You Light Up My Life',
-    weeksAtOne: 10
-  },
-  {
-    decade: '1980s',
-    artist: 'Olivia Newton-John',
-    song: 'Physical',
-    weeksAtOne: 10
-  },
-  {
-    decade: '1990s',
-    artist: 'Mariah Carey',
-    song: 'One Sweet Day',
-    weeksAtOne: 16
-  }
-];
-
-// Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
-
-let uri = 'mongodb://user:pass@host:port/dbname';
-
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, client) {
-
-  if(err) throw err;
-
-  /*
-   * Get the database from the client. Nothing is required to create a
-   * new database, it is created automatically when we insert.
-   */
-
-  let db = client.db('dbname')
-
-  /*
-   * First we'll add a few songs. Nothing is required to create the
-   * songs collection; it is created automatically when we insert.
-   */
-
-  let songs = db.collection('songs');
-
-   // Note that the insert method can take either an array or a dict.
-
-  songs.insert(seedData, function(err, result) {
-
-    if(err) throw err;
-
-    /*
-     * Then we need to give Boyz II Men credit for their contribution
-     * to the hit "One Sweet Day".
-     */
-
-    songs.update(
-      { song: 'One Sweet Day' },
-      { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
-      function (err, result) {
-
-        if(err) throw err;
-
-        /*
-         * Finally we run a query which returns all the hits that spend 10 or
-         * more weeks at number 1.
-         */
-
-        songs.find({ weeksAtOne : { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
-
-          if(err) throw err;
-
-          docs.forEach(function (doc) {
-            console.log(
-              'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] +
-              ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
-            );
-          });
-
-          // Since this is an example, we'll clean up after ourselves.
-          songs.drop(function (err) {
-            if(err) throw err;
-
-            // Only close the connection when your app is terminating.
-            client.close(function (err) {
-              if(err) throw err;
-            });
-          });
-        });
-      }
-    );
-  });
-});
 	
-	// mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', (err, client)=>{
-	// 	console.log("HOORAH")
-	// 	        // delete req.body._id; // for safety reasons
-	// 	 let  db = client.db('MyDb') 
-	// 	 let collection = db.collection('users')
+	mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', (err, client)=>{
+		let  db = client.db('UsersDb') 
+		let collection = db.collection('users')
 		 
-	// 	 collection.insertOne({ id: 1, phone_number: phoneNumber, lat: lat, long: long }, (err, result)=>{
-	// 	 	if(err)console.log('error inserting')
-	// 	 		console.log("data inserted", result)
-	// 	 })   
- //   });
+		collection.insertOne({ id: 1, phone_number: phoneNumber, lat: lat, long: long }, (err, result)=>{
+			if(err)console.log('error inserting')
+		 		console.log("data inserted", result)
+		})   
+   });
 
 
  	// const message = "This is your intro TEXT...Text 'Stop' at any time to stop receving SUNS°ET forecasts."
