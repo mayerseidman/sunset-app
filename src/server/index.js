7f32e0c26cec71ef8a25229c9420ae9dadc7c3d2
mayerseidman <mzseidman@gmail.com>
2020-04-02 07:38:22 -0700

    changes

--- src/server/index.js
+++ src/server/index.js
@@ -242,7 +242,7 @@ const sunsetwx = new SunsetWx({
 // var MongoClient = require('mongodb').MongoClient;
 // var url = "";
 
-var job = new CronJob('49 21 * * *', function() { 
+var job = new CronJob('0 12 * * *', function() { 
 	mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', (err, client)=>{
 		let  db = client.db('heroku_9v9cjldm') 
 		let users = db.collection('users')
